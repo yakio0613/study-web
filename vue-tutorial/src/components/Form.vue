414a9db677e135a793a97fdb0294287d1e1bf0ec
hasunuma yoshio <yakki0613@gmail.com>
2019-02-17 15:43:36 +0900

    add vuex tutorial

---
+++ vue-tutorial/src/components/Form.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    Formページ
+    <HeadComp></HeadComp>
+    <TextareaComp></TextareaComp>
+    <StringComp></StringComp>
+    <button v-on:click="buttonAction">{{button}}</button>
+  </div>
+</template>
+
+<script>
+import HeadComp from '@/components/modules/HeadComp'
+import TextareaComp from '@/components/modules/TextareaComp'
+import StringComp from '@/components/modules/StringComp'
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  name: 'Form',
+  data () {
+    return {
+      button: '確認'
+    }
+  },
+  ...mapActions('Form', {
+    'buttonAction': 'buttonAction'
+  }),
+  components: {
+    HeadComp,
+    TextareaComp,
+    StringComp
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
